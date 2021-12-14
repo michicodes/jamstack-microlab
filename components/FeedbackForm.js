9405d7f7258eb29864ba7cd52d45a0f95f3ca8f9
Michael Weilbächer <michael.weilbaecher@senacor.com>
2021-12-14 14:29:19 +0100

    add index page

---
+++ components/FeedbackForm.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import logo from '../public/supreme_senacor.png'
+
+const FeedbackForm = () => {
+    return (<>
+        <form className="w-full max-w-lg" netlify="true">
+            <Image src={logo}/>
+            <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+                    </label>
+                    <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="                        Which Senacor merch do you wanna see next?
+"/>
+                    <button className="bg-alizarin-crimson text-white font-bold py-3 px-4" type="submit">whish for it!</button>
+                </div>
+            </div>
+        </form>
+    </>)
+
+}
+
+export default FeedbackForm
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-import Header from '@components/Header'
+
 import Footer from '@components/Footer'
 import Content from '@components/Content'
 
+
+
+import FeedbackForm from '@components/FeedbackForm'
+
 export default function Home() {
-    return (<>
-            <Head>
-                <title>Next.js Starter!</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <Content>
-                <main>
-                    <Header title="Welcome to my app!"/>
-                    <p className="description">
-                        Get started by editing <code>pages/index.js</code>
-                    </p>
-                    <ul>
-                        <li><Link href="/products"><a>Products</a></Link></li>
-                    </ul>
-                </main>
-            </Content>
-            <Footer/>
-        </>
-    )
+  return (
+    <div className="mx-auto flex justify-center items-center h-screen">
+      <Head>
+        <title>senacor.shop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <FeedbackForm />
+      </main>
+      <Footer />
+    </div>
+  )
 }
