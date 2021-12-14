efe3e39975b3704ef40f080ca7fbb28aba8edb45
Michael Weilbächer <michael.weilbaecher@senacor.com>
2021-12-14 14:48:48 +0100

    try netlify forms

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -3,7 +3,7 @@ import logo from '../public/supreme_senacor.png'
 
 const FeedbackForm = () => {
     return (<>
-        <form className="w-full max-w-lg" netlify="true">
+        <form className="w-full max-w-lg" name="Merch" method="POST" data-netlify="true">
             <Image src={logo}/>
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
