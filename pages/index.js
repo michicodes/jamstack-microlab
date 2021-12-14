e573a87526b066c84e740383a28d40ce46d792c8
Thomas Maqua <thomas.maqua@gcp.deuba.com>
2021-12-14 15:04:07 +0100

    Add meta description

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,21 @@ import Footer from '@components/Footer'
 import Content from '@components/Content'
 
 
-
 import FeedbackForm from '@components/FeedbackForm'
 
 export default function Home() {
-  return (
-    <div className="mx-auto flex justify-center items-center h-screen">
-      <Head>
-        <title>senacor.shop</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="mx-auto flex justify-center items-center h-screen">
+            <Head>
+                <title>senacor.shop</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <meta name="description">Senacor Shop</meta>
+            </Head>
 
-      <main>
-        <FeedbackForm />
-      </main>
-      <Footer />
-    </div>
-  )
+            <main>
+                <FeedbackForm/>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
