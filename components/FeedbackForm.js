842ed89e9797dd04e31a1c8c8ac74ba60a7c1dc0
Michael Weilbächer <michael.weilbaecher@senacor.com>
2021-12-14 15:08:44 +0100

    adds hidden input field

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -3,7 +3,8 @@ import logo from '../public/supreme_senacor.png'
 
 const FeedbackForm = () => {
     return (<>
-        <form className="w-full max-w-lg" name="Merch" method="POST" data-netlify="true">
+        <form className="w-full max-w-lg" action="/" name="merch" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="merch" />
             <Image src={logo} alt="Senacor" />
             <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
