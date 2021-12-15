f4d1d8d21b7c082f24e3f62cb63036a44b2ac1bb
Thomas Maqua <thomas.maqua@gcp.deuba.com>
2021-12-15 12:22:31 +0100

    Edge Handlers experiment

---
+++ edge-handlers/deny-secret.js
@@ -0,0 +1,12 @@
+export function onRequest(event) {
+    if (event.requestMeta.url.pathname === "/secret") {
+        event.replaceResponse(
+            new Response("<h1>Access denied</h1>", {
+                headers: {
+                    "content-type": "text/html",
+                },
+                status: 404,
+            })
+        );
+    }
+}
